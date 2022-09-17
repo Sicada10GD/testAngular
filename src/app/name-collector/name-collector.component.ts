609cff5d3be64031ab6bfd95453b753805b27832
Sicada10GD <91421374+Sicada10GD@users.noreply.github.com>
2022-09-18 00:12:25 +0530

    assignment 2

---
+++ src/app/name-collector/name-collector.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-collector',
+  templateUrl: './name-collector.component.html',
+  styleUrls: ['./name-collector.component.css'],
+})
+export class NameCollectorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+  fName = '';
+  buttonStatus() {
+    if (this.fName.trim().length > 0) return false;
+    else return true;
+  }
+}
